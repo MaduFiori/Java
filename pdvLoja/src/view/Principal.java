0dc0ad78060bac4c15621c4e5d0e9e8edabda0e6
Maria Eduarda Fiori <fiori22eduarda@gmail.com>
2022-08-05 12:07:02 -0300

    PDV loja tintas

--- pdvLoja/src/view/Principal.java
+++ pdvLoja/src/view/Principal.java
@@ -17,6 +17,13 @@ import javax.swing.border.EmptyBorder;
 import java.awt.Font;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.awt.Window.Type;
+import java.awt.Dialog.ModalExclusionType;
+import java.awt.Color;
+import javax.swing.border.BevelBorder;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+import java.awt.ComponentOrientation;
 
 public class Principal extends JFrame {
 
@@ -56,18 +63,20 @@ public class Principal extends JFrame {
 		        lblData.setText(formatador.format(data));
 			}
 		});
-		setIconImage(Toolkit.getDefaultToolkit().getImage(Principal.class.getResource("/img/favicon.png")));
-		setTitle("Lojinha");
+		setIconImage(Toolkit.getDefaultToolkit().getImage(Principal.class.getResource("/img/balde-de-tinta (1).png")));
+		setTitle("Tintas Maria");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 640, 480);
 		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setBackground(Color.WHITE);
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
 		JButton btnNewButton = new JButton("");
+		btnNewButton.setDefaultCapable(false);
+		btnNewButton.setContentAreaFilled(false);
 		btnNewButton.addActionListener(new ActionListener() {
-			//evento clicar no botão
+			//evento clicar no botï¿½o
 			public void actionPerformed(ActionEvent e) {
 				// Link para o jDialog
 				Usuarios usuarios = new Usuarios();
@@ -76,11 +85,13 @@ public class Principal extends JFrame {
 		});
 		btnNewButton.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
 		btnNewButton.setToolTipText("Usu\u00E1rios");
-		btnNewButton.setIcon(new ImageIcon(Principal.class.getResource("/img/users.png")));
+		btnNewButton.setIcon(new ImageIcon(Principal.class.getResource("/img/useruser.png")));
 		btnNewButton.setBounds(20, 23, 128, 128);
 		contentPane.add(btnNewButton);
 		
 		JButton btnNewButton_1 = new JButton("");
+		btnNewButton_1.setContentAreaFilled(false);
+		btnNewButton_1.setBorderPainted(false);
 		btnNewButton_1.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				Fornecedores fornecedores = new Fornecedores();
@@ -89,11 +100,13 @@ public class Principal extends JFrame {
 		});
 		btnNewButton_1.setToolTipText("Fornecedores");
 		btnNewButton_1.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
-		btnNewButton_1.setIcon(new ImageIcon(Principal.class.getResource("/img/supliers.png")));
+		btnNewButton_1.setIcon(new ImageIcon(Principal.class.getResource("/img/forne.png")));
 		btnNewButton_1.setBounds(172, 23, 128, 128);
 		contentPane.add(btnNewButton_1);
 		
 		JButton btnNewButton_1_1 = new JButton("");
+		btnNewButton_1_1.setContentAreaFilled(false);
+		btnNewButton_1_1.setBorderPainted(false);
 		btnNewButton_1_1.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				Clientes clientes = new Clientes();
@@ -102,11 +115,13 @@ public class Principal extends JFrame {
 		});
 		btnNewButton_1_1.setToolTipText("Clientes");
 		btnNewButton_1_1.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
-		btnNewButton_1_1.setIcon(new ImageIcon(Principal.class.getResource("/img/clientes.png")));
-		btnNewButton_1_1.setBounds(472, 23, 128, 128);
+		btnNewButton_1_1.setIcon(new ImageIcon(Principal.class.getResource("/img/clientenovo.png")));
+		btnNewButton_1_1.setBounds(461, 23, 128, 128);
 		contentPane.add(btnNewButton_1_1);
 		
 		JButton btnNewButton_2 = new JButton("");
+		btnNewButton_2.setDefaultCapable(false);
+		btnNewButton_2.setContentAreaFilled(false);
 		btnNewButton_2.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				Produtos produtos = new Produtos();
@@ -115,11 +130,12 @@ public class Principal extends JFrame {
 		});
 		btnNewButton_2.setToolTipText("Produtos");
 		btnNewButton_2.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
-		btnNewButton_2.setIcon(new ImageIcon(Principal.class.getResource("/img/products.png")));
+		btnNewButton_2.setIcon(new ImageIcon(Principal.class.getResource("/img/produtosicon.png")));
 		btnNewButton_2.setBounds(323, 23, 128, 128);
 		contentPane.add(btnNewButton_2);
 		
 		JButton btnNewButton_3 = new JButton("");
+		btnNewButton_3.setContentAreaFilled(false);
 		btnNewButton_3.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				Vendas vendas = new Vendas();
@@ -128,33 +144,39 @@ public class Principal extends JFrame {
 		});
 		btnNewButton_3.setToolTipText("PDV");
 		btnNewButton_3.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
-		btnNewButton_3.setIcon(new ImageIcon(Principal.class.getResource("/img/cashbox.png")));
+		btnNewButton_3.setIcon(new ImageIcon(Principal.class.getResource("/img/caixapdv.png")));
 		btnNewButton_3.setBounds(20, 177, 128, 128);
 		contentPane.add(btnNewButton_3);
 		
 		JButton btnNewButton_1_2 = new JButton("");
+		btnNewButton_1_2.setContentAreaFilled(false);
+		btnNewButton_1_2.setBorderPainted(false);
 		btnNewButton_1_2.setToolTipText("Relat\u00F3rios");
 		btnNewButton_1_2.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
-		btnNewButton_1_2.setIcon(new ImageIcon(Principal.class.getResource("/img/report.png")));
+		btnNewButton_1_2.setIcon(new ImageIcon(Principal.class.getResource("/img/relatorio.png")));
 		btnNewButton_1_2.setBounds(172, 177, 128, 128);
 		contentPane.add(btnNewButton_1_2);
 		
 		JButton btnNewButton_2_1 = new JButton("");
+		btnNewButton_2_1.setBorderPainted(false);
+		btnNewButton_2_1.setContentAreaFilled(false);
 		btnNewButton_2_1.setToolTipText("Ferramentas");
 		btnNewButton_2_1.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
-		btnNewButton_2_1.setIcon(new ImageIcon(Principal.class.getResource("/img/tools.png")));
+		btnNewButton_2_1.setIcon(new ImageIcon(Principal.class.getResource("/img/conf.png")));
 		btnNewButton_2_1.setBounds(323, 177, 128, 128);
 		contentPane.add(btnNewButton_2_1);
 		
 		JButton btnNewButton_1_1_1 = new JButton("");
+		btnNewButton_1_1_1.setContentAreaFilled(false);
+		btnNewButton_1_1_1.setBorderPainted(false);
 		btnNewButton_1_1_1.setToolTipText("Sobre");
 		btnNewButton_1_1_1.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
-		btnNewButton_1_1_1.setIcon(new ImageIcon(Principal.class.getResource("/img/about.png")));
+		btnNewButton_1_1_1.setIcon(new ImageIcon(Principal.class.getResource("/img/chat.png")));
 		btnNewButton_1_1_1.setBounds(472, 177, 128, 128);
 		contentPane.add(btnNewButton_1_1_1);
 		
 		JPanel panel = new JPanel();
-		panel.setBackground(SystemColor.textHighlight);
+		panel.setBackground(Color.LIGHT_GRAY);
 		panel.setBounds(0, 388, 624, 53);
 		contentPane.add(panel);
 		panel.setLayout(null);
@@ -164,5 +186,12 @@ public class Principal extends JFrame {
 		lblData.setFont(new Font("Tahoma", Font.BOLD, 14));
 		lblData.setBounds(325, 11, 289, 31);
 		panel.add(lblData);
+		
+		JLabel lblNewLabel = new JLabel("");
+		lblNewLabel.setBounds(550, 351, 64, 64);
+		contentPane.add(lblNewLabel);
+		lblNewLabel.setCursor(Cursor.getPredefinedCursor(Cursor.DEFAULT_CURSOR));
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel.setIcon(new ImageIcon(Principal.class.getResource("/img/pincel.png")));
 	}
 }
